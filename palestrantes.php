0860e03dafd4d190096ea8328b6d58c300c502da
gabrielbse <gabrielbatista001@gmail.com>
2017-10-05 16:09:02 -0300

    adição do carrossel

--- palestrantes.php
+++ palestrantes.php
@@ -4,7 +4,15 @@
 	<body>
 		<?php  include "./elementos/nav.php"; ?>
 		<main>
-
+			<div class="deposito">
+				<br>
+				<div class="carousel carousel-slider">
+					<div class="carousel-item" ><img src="https://lorempixel.com/800/400/food/1"></div>
+					<a class="carousel-item" href="#two!"><img src="https://lorempixel.com/800/400/food/2"></a>
+					<a class="carousel-item" href="#three!"><img src="https://lorempixel.com/800/400/food/3"></a>
+					<a class="carousel-item" href="#four!"><img src="https://lorempixel.com/800/400/food/4"></a>
+				</div>
+			</div>
 		</main>
 		<?php  include "./elementos/footer.php"; ?>
 		<?php  include "./elementos/scripts.php"; ?>
