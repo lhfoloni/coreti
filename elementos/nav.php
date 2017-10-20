9092df92cc85e5d98225964742c3d4fb37454f93
gabrielbse <gabrielbatista001@gmail.com>
2017-10-20 14:54:16 -0300

    adição redes sociais

--- elementos/nav.php
+++ elementos/nav.php
@@ -4,17 +4,20 @@
     <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
 
     <ul class="right hide-on-med-and-down">
+      <li><a href="#" onclick="window.open(&quot;https://goo.gl/GfwtEf&quot;);"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a></li>
+      <li><a href="#" onclick="window.open(&quot;https://goo.gl/uUPJHJ&quot;);"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a></li>
       <li><a href="index.php"><i class="fa fa-university fa-2x" aria-hidden="true"></i><span>     Evento</span></a></li>
       <li><a href="palestras.php"><i class="fa fa-calendar-check-o fa-2x" aria-hidden="true"></i><span     >Palestras</span></a></li>
       <li><a href="mini.php"><i class="fa fa-book fa-2x" aria-hidden="true"></i><span>     Minicurso / Oficina</span></a></li>
       <li><a href="palestrantes.php"><i class="fa fa-user-o fa-2x" aria-hidden="true"></i><span>     Palestrantes</span></a></li>
       <li><a href="inscricao.php"><i class="fa fa-pencil fa-2x" aria-hidden="true"></i><span>     Inscrições</span></a></li>
       <li><a href="localizacao.php"><i class="fa fa-map-o fa-2x" aria-hidden="true"></i><span>     Localização</span></a></li>
-       <li><a href="contato.php"><i class="fa fa-phone fa-2x" aria-hidden="true"></i><span>     Contato</span></a></li>
+      <li><a href="contato.php"><i class="fa fa-phone fa-2x" aria-hidden="true"></i><span>     Contato</span></a></li>
     </ul>
 </div>
     <ul class="side-nav" id="mobile-demo">
-          
+      <li><a href="#" onclick="window.open(&quot;https://goo.gl/GfwtEf&quot;);"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i><span>     Nosso Facebook</span></a></li>
+      <li><a href="#" onclick="window.open(&quot;https://goo.gl/uUPJHJ&quot;);"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i><span>     Nosso Instagram</span></a></li>    
       <li><a href="index.php"><i class="fa fa-university fa-2x" aria-hidden="true"></i><span>     Evento</span></a></li>
       <li><a href="palestras.php"><i class="fa fa-calendar-check-o fa-2x" aria-hidden="true"></i><span     >Palestras</span></a></li>
       <li><a href="mini.php"><i class="fa fa-book fa-2x" aria-hidden="true"></i><span>     Minicursos</span></a></li>
