15af54e5e7ed269fc8a5893ff0129c6ae7691bc2
gabrielbse <gabrielbatista001@gmail.com>
2017-10-31 09:37:43 -0300

    atualização

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
 				</div>
 			</div>			
 			<div class="deposito">
-				<p class="flow-text" align="justify">O I Congresso Regional de TI (I CoReTI) tem como objetivo proporcionar aos alunos do curso de Sistemas de Informação da UESB uma maior integração, seja com alunos do próprio Campus de Jequié, ou alunos de outras Instituições de Ensino Superior com cursos voltados à área de Tecnologia da Informação (TI), buscando a atualização nas tecnologias, métodos e ferramentas utilizadas no projeto, desenvolvimento e gerenciamento de Sistemas de Informação. Busca-se esta ação por meio de palestras e minicursos aplicados por alunos e professores da UESB. Os minicursos oferecidos durante o evento serão ministrados por professores e alunos do curso de Sistemas de Informação, Professores e alunos convidados, de outras instituições, proporcionando a estes a prática no ensino e pesquisa.</p>
+				<p class="flow-text" align="justify">O I Congresso Regional de TI (I CoRe TI) tem como objetivo proporcionar aos alunos do curso de Sistemas de Informação da UESB uma maior integração, seja com alunos do próprio Campus de Jequié, ou alunos de outras Instituições de Ensino Superior com cursos voltados à área de Tecnologia da Informação (TI), buscando sempre a atualização no que diz respeito a novas tecnologias, métodos e ferramentas utilizadas no projeto, desenvolvimento e gerenciamento de Sistemas de Informação.
+				O evento será realizado nos dias 8,9 e 10 do mês de novembro na UESB campus Jequié, no anfiteatro do pavilhão Professor Manoel Sarmento, e visa atingir o público da cidade e das cidades circunvizinhas. Durante os dias do evento estarão acontecendo palestras e minicursos das mais diversas áreas, como por exemplo, palestras de empreendedorismo, robótica, redes e segurança, minicursos de introdução ao Arduíno, Segurança Web, BootStrap, Métricas de ponto de função, Introdução ao Linux e Photoshop.</p>
 		</div>
 
 		</main>
